b47c27a8f4ee31e4da1bb9ad2d283b56b90aa852
Theowhocodes <williams2theodore@gmail.com>
2023-02-16 21:56:18 -0500

    added database connection

--- java/database/user.sql
+++ java/database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
-WITH PASSWORD 'finalcapstone';
+CREATE USER minority_connect_owner
+WITH PASSWORD 'minorityconnect';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO minority_connect_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO minority_connect_owner;
 
-CREATE USER final_capstone_appuser
-WITH PASSWORD 'finalcapstone';
+CREATE USER minority_connect_appuser
+WITH PASSWORD 'minorityconnect';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_appuser;
+TO minority_connect_appuser;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_appuser;
+TO minority_connect_appuser;
